0103a8c8b49bb33a0130a44382c55818bdd370d7
mattv3 <msamoy3@gmail.com>
2025-04-19 21:33:06 -0700

    working on carousel

---
+++ script.js
@@ -0,0 +1,17 @@
+// new Swiper('.carousel-container', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // pagination 
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+  
