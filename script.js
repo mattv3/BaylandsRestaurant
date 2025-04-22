e55ce99d68655b92afcf896e486daf06171c9cb2
mattv3 <msamoy3@gmail.com>
2025-04-21 21:16:38 -0700

    carousel done!

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-// new Swiper('.carousel-container', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
+new Swiper('.carousel-container', {
+  loop: true,
+  spaceBetween: 30,
 
-//   // pagination 
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
+  // pagination 
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
 
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+//   screen sizes
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+  }
+});
   
